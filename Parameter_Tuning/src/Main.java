56314412b086de20185fd4dc6c321b5a01b2eddd
Pantelis Rodis <32998445+rodispantelis@users.noreply.github.com>
2022-08-11 18:41:07 +0300

    Update Main.java

--- Parameter_Tuning/src/Main.java
+++ Parameter_Tuning/src/Main.java
@@ -5,6 +5,21 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+/*
+    This is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+	https://www.gnu.org/licenses/agpl-3.0.txt
+	
+	Published by Pantelis Rodis, 2021
+*/	
+
 public class Main {
     
 	int population=-1, generations=-1, supergenerations=-1;
